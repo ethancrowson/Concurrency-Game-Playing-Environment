fa835a04adf1bd91e786f0b1a62ffb882070b4af
Ethan Crowson <zkac173@live.rhul.ac.uk>
2023-10-25 15:51:33 +0100

    Chess Board GUI creation.

--- Code/cs3821fyp/src/main/java/com/ecrowson/Main.java
+++ Code/cs3821fyp/src/main/java/com/ecrowson/Main.java
@@ -2,19 +2,30 @@ package com.ecrowson;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.paint.*;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.shape.*;
 
 public class Main extends Application {
+    GridPane board = new GridPane();
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        for (int file=0; file<8; file++){
+            for (int rank=0; rank<8; rank++){
+                Rectangle tile = new Rectangle();
+                boolean isWhite = (file+rank)%2 == 0;
+                if (isWhite) tile.setFill(Color.rgb(238,238,210));
+                else tile.setFill(Color.rgb(118,150,86));
+                board.add(tile, file, rank);
+                tile.widthProperty().bind(board.widthProperty().divide(8));
+                tile.heightProperty().bind(board.heightProperty().divide(8));
+            }
+        }
+        Scene scene = new Scene(board, 480, 480);
         stage.setScene(scene);
+        stage.setTitle("Chess");
         stage.show();
     }
 
