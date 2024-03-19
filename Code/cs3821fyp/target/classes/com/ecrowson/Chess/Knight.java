820d2e94ab38faa9c8d84b222ce94a8067509bd5
Ethan Crowson <zkac173@live.rhul.ac.uk>
2024-03-19 22:22:02 +0000

    Completed Knight piece movement

--- Code/cs3821fyp/target/classes/com/ecrowson/Chess/Knight.java
+++ Code/cs3821fyp/target/classes/com/ecrowson/Chess/Knight.java
@@ -9,6 +9,97 @@ public class Knight extends Piece {
     }
     public char getType(){return 'N';}
     public ArrayList<Tile> getMoves(Tile[][] board, int file, int rank){
-        return null;
+        ArrayList<Tile> pMoves = new ArrayList<>();
+            if (rank > 1){
+                //UP LEFT
+                if (file > 0){
+                    if (!board[file-1][rank-2].isOccupied()){
+                        pMoves.add(board[file-1][rank-2]);
+                    }else {
+                        if (board[file-1][rank-2].getPiece().isWhite != this.isWhite){
+                            pMoves.add(board[file-1][rank-2]);
+                        }
+                    }
+                }
+                //UP RIGHT
+                if (file < 7){
+                    if (!board[file+1][rank-2].isOccupied()){
+                        pMoves.add(board[file+1][rank-2]);
+                    }else {
+                        if (board[file+1][rank-2].getPiece().isWhite != this.isWhite){
+                            pMoves.add(board[file+1][rank-2]);
+                        }
+                    }
+                }
+            }
+
+            if (file < 6) {
+                //RIGHT UP
+                if (rank > 0){
+                    if (!board[file+2][rank-1].isOccupied()){
+                        pMoves.add(board[file+2][rank-1]);
+                    }else {
+                        if (board[file+2][rank-1].getPiece().isWhite != this.isWhite){
+                            pMoves.add(board[file+2][rank-1]);
+                        }
+                    }
+                }
+                //RIGHT DOWN
+                if (rank < 7){
+                    if (!board[file+2][rank+1].isOccupied()){
+                        pMoves.add(board[file+2][rank+1]);
+                    }else {
+                        if (board[file+2][rank+1].getPiece().isWhite != this.isWhite){
+                            pMoves.add(board[file+2][rank+1]);
+                        }
+                    }
+                }
+            }
+
+            if (rank < 6) {
+                //DOWN RIGHT
+                if (file < 7){
+                    if (!board[file+1][rank+2].isOccupied()){
+                        pMoves.add(board[file+1][rank+2]);
+                    }else {
+                        if (board[file+1][rank+2].getPiece().isWhite != this.isWhite){
+                            pMoves.add(board[file+1][rank+2]);
+                        }
+                    }
+                }
+                //DOWN LEFT
+                if (file > 0){
+                    if (!board[file-1][rank+2].isOccupied()){
+                        pMoves.add(board[file-1][rank+2]);
+                    }else {
+                        if (board[file-1][rank+2].getPiece().isWhite != this.isWhite){
+                            pMoves.add(board[file-1][rank+2]);
+                        }
+                    }
+                }
+            }
+            if (file > 1){
+                //LEFT DOWN
+                if (rank < 7){
+                    if (!board[file-2][rank+1].isOccupied()){
+                        pMoves.add(board[file-2][rank+1]);
+                    }else {
+                        if (board[file-2][rank+1].getPiece().isWhite != this.isWhite){
+                            pMoves.add(board[file-2][rank+1]);
+                        }
+                    }
+                }
+                //LEFT UP
+                if (rank > 0){
+                    if (!board[file-2][rank-1].isOccupied()){
+                        pMoves.add(board[file-2][rank-1]);
+                    }else {
+                        if (board[file-2][rank-1].getPiece().isWhite != this.isWhite){
+                            pMoves.add(board[file-2][rank-1]);
+                        }
+                    }
+                }
+            }
+        return pMoves;
     }
 }
